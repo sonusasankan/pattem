4fc3cd789b6b66eeb9847a4bf30e197de065ac97
sonusasankan <sonusparsham@gmail.com>
2019-02-14 23:47:13 +0530

    implement search query function

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 
-// function fetchData(url) {
-//     return new Promise((resolve, reject) => {
-//       const xhr = new XMLHttpRequest();
-//       xhr.open("GET", 'https://newsapi.org/v2/everything?q=reactjs&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=1');
-//       xhr.onload = () => resolve(JSON.parse(xhr.responseText));
-//       xhr.onerror = () => reject(xhr.statusText);
-//       xhr.send();
-//     });
-//   }
+var url = new URL("https://newsapi.org/v2/everything?q=bitcoin&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=1");
+var query_string = url.search;
+var search_params = new URLSearchParams(query_string); 
+var pageSize = search_params.get('pageSize');
 
-// console.log(fetchData());
-
-var data;
-var url_string = "https://newsapi.org/v2/everything?q=reactjs&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=1";
-var url = new URL(url_string);
-var c = url.searchParams.get("name");
-console.log(c)
+let root = document.getElementById("news")  
 
 function imageExists(url, callback) {
     var img = new Image();
@@ -24,34 +13,60 @@ function imageExists(url, callback) {
     img.src = url;
 }
 
-function loadDoc(data) {
+function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        let item
-        let root = document.getElementById("news")  
-        let data = JSON.parse(this.responseText)
-        data.articles.map(article => {
-            item = document.createElement("DIV")
-            // let details = document.createElement("DIV")
-            // details.setAttribute('class', 'news-details')
-            // let title = document.createElement("H2")
-            item.setAttribute('class', 'news-list-item');
-            let img = document.createElement("IMG")
-            let imgExist = imageExists(article.urlToImage, function(exists) {
-                return exists
-            });  
-            item.innerHTML = "<div class='news-img'>"+ "<img src="+(imgExist? "https://via.placeholder.com/150":article.urlToImage)+">"+"</div>"+"<div class='news-details'>" + "<h1 class='news-title'>" + article.title +"</h1>" + "<p>"+article.description+"</p>"+"</div>";
-            root.appendChild(item);
-        })
+        let data = JSON.parse(this.responseText);
+        var output = '';
+        for(var i in data.articles){
+            output += '<div class="news-list-item">'+
+            '<div class="news-img">'+ 
+            '<img src="'+ data.articles[i].urlToImage +'" />'+
+            '</div>'+
+            '<div class="news-details">' + 
+            '<h1 class="news-title">' + data.articles[i].title + 
+            '</h1>' + 
+            '<p>'+ data.articles[i].description+ '</p>'+
+            '</div>'+
+            '</div>'
+        }
+        root.innerHTML = output;
+
+        if(data.totalResults == 0){
+            root.innerHTML = '<div class="progress-loader"><h1>OOPS!! No Results</h1></div>';
+        }
+
+      }else if(this.readyState == 3){
+        root.innerHTML = '<div class="progress-loader"><img src="preloader.gif"/></div>';
+      }else{
+          root.innerHTML = '<div class="progress-loader"><img src="preloader.gif"/></div>';
+
       }
     };
-    xhttp.open("GET", url_string, true);
+    xhttp.open("GET", url, true);
     xhttp.send();
   }
-loadDoc(data);
+loadDoc();
+
+// setInterval(function(){
+//     loadDoc()
+// }, 30000)
 
 function validateForm(e){
     e.preventDefault()
-    console.log('submitted')
+    let value = e.target.search.value;
+    search_params.set('q', value);
+    url.search = search_params.toString();
+    url.toString();
+    loadDoc()
 }
+
+onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        // search_params.set('pageSize', pageSize++);
+        // url.search = search_params.toString();
+        // url.toString();
+        // loadDoc()
+    }
+};
